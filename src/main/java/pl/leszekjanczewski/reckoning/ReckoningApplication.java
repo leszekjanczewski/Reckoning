88d1ad5b3b4845e97f8e445f2c71004c1e12d862
leszekjanczewski <leszek.janczewski@gmail.com>
2019-01-13 11:45:15 +0100

    change spring into boot spring

---
+++ src/main/java/pl/leszekjanczewski/reckoning/ReckoningApplication.java
@@ -0,0 +1,13 @@
+package pl.leszekjanczewski.reckoning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReckoningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReckoningApplication.class, args);
+	}
+
+}
