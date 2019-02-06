daa146ea24c1f2c5ea5326a364607e5109a48fe8
leszekjanczewski <leszek.janczewski@gmail.com>
2019-02-06 14:46:21 +0100

    correct pom.xml and Security.config - to working with travic-cl

--- src/main/java/pl/leszekjanczewski/reckoning/app/SecurityConfig.java
+++ src/main/java/pl/leszekjanczewski/reckoning/app/SecurityConfig.java
@@ -2,11 +2,9 @@ package pl.leszekjanczewski.reckoning.app;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
